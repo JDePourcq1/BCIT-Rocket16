ffcce5ade23700db085ac676b4ceb880df91e460
ShaggyBlarneys <mkearney6@my.bcit.ca>
2016-05-25 17:56:12 -0700

    Fixed the resize bug again

--- Week5/KittyCatSpaceMap v0.2.5/js/gameBoard.0.3.js
+++ Week5/KittyCatSpaceMap v0.2.5/js/gameBoard.0.3.js
@@ -569,6 +569,9 @@ function centerBoard() {
 
 //Zooms the game board in relation to the canvas
 function zoomBoard() {
-	var zoomFactor = canvas.width <= canvas.height ? canvas.width / 480 : canvas.height / 320;
+	var lbounds = paper.project.activeLayer.bounds;
+	var vbounds = paper.view.bounds;
+	var zoomFactor = lbounds.width <= lbounds.height ? vbounds.width / 480 : vbounds.height / 320;
+	console.log(zoomFactor);
 	paper.view.zoom = zoomFactor;
 }
